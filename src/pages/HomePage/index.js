1b3bf56e3d41145f6ac2053c881f0db152234bda
zhuyuyi <yyzhu@che300.com>
2020-08-16 01:00:12 +0800

    23

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -6,7 +6,7 @@ import { Button } from 'antd';
 class HomePage extends Component{
 
     render () {
-        let b = 1;
+        let c = 1;
         return (
             <div>
                 <Button type="primary">sssss</Button>
