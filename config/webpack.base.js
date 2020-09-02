16eba54ec2b1d80f91fc6ba2cddd873aaad6f72d
zhuyuyi <yyzhu@che300.com>
2020-09-02 18:30:47 +0800

    上传代码

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,11 +1,12 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 打出单独的css包
 
 module.exports = {
     entry: {
         index: './src/main.js',
-        another: './src/another-module.js'
+        another: './src/another-module.js',
     },
     optimization: {
         splitChunks: {
@@ -14,9 +15,9 @@ module.exports = {
             cacheGroups: {
                 vendors: {
                     test: /[\\/]node_modules[\\/]/,
-                    priority: 1
-                }
-            }
+                    priority: 1,
+                },
+            },
         },
     },
     module: {
@@ -24,45 +25,49 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: [{
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],  //babel-loader高版本对应新的预设 @babel/preset-env和@babel/preset-react
-                        plugins: [
-                            [
-                                "@babel/plugin-proposal-class-properties",
-                                {
-                                    "loose": true
-                                }
-                            ]
-                        ]
-                    }
-                }]
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'], //babel-loader高版本对应新的预设 @babel/preset-env和@babel/preset-react
+                            plugins: [
+                                [
+                                    '@babel/plugin-proposal-class-properties',
+                                    {
+                                        loose: true,
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                ],
             },
             {
                 test: /\.less$/,
                 use: [
-                    'style-loader',
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
                     {
                         loader: 'css-loader',
                         options: {
                             modules: {
-                                localIdentName: "electron-tools-[local]-[hash:5]",
-                            }
-                        }
+                                localIdentName: 'electron-tools-[local]-[hash:5]',
+                            },
+                        },
                     },
                     {
                         loader: 'less-loader',
-                    }
-                ]
+                    },
+                ],
                 // 增加 'less-loader' ，注意顺序
             },
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                    // 'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)/,
@@ -70,29 +75,32 @@ module.exports = {
                     loader: 'url-loader',
                     options: {
                         outputPath: 'images/', // 图片输出的路径
-                        limit: 10 * 1024
-                    }
-                }
+                        limit: 10 * 1024,
+                    },
+                },
             },
             //处理 html 代码中 <img src="..."/> 的形式
             {
                 test: /\.html$/,
-                use: 'html-withimg-loader'
-            }
-        ]
+                use: 'html-withimg-loader',
+            },
+        ],
     },
     plugins: [
         //创建入口文件html
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './index.html'
+            template: './index.html',
         }),
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn|ja/),
+        new MiniCssExtractPlugin({
+            filename: 'css/index.[hash].css',
+        }),
     ],
     resolve: {
         extensions: ['.js', '.jsx', '.json'],
         alias: {
-            "@": path.join(__dirname, "./../src") // 这样@符号就表示项目根目录中src这一层路径
+            '@': path.join(__dirname, './../src'), // 这样@符号就表示项目根目录中src这一层路径
         },
-    }
-}
+    },
+};
